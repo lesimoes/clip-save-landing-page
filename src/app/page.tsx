9ebc7950a73908123c0c26b62148ddfdefe39ae5
Leandro Simões <lesimoes0@gmail.com>
2025-04-06 19:25:56 -0300

    change to v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,9 @@ const Highlight = ({ children }: { children: string }) => {
 };
 
 export default function Home() {
+  const appLink =
+    'https://github.com/lesimoes/clip-save/releases/download/v2.0.0/clip-save.zip';
+
   return (
     <main className="bg-white text-black min-h-screen font-sans">
       <header className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between px-6 md:px-12 py-4">
@@ -22,7 +25,7 @@ export default function Home() {
           ClipSave
         </div>
         <a
-          href="https://github.com/lesimoes/clip-save/releases/download/v1.0.1/clip-save.zip"
+          href={appLink}
           target="_blank"
           rel="noopener noreferrer"
           className="border border-[#F2502F] text-[#F2502F] px-4 py-2 rounded-full text-sm hover:bg-[#F2502F] hover:text-white transition-all"
@@ -53,7 +56,7 @@ export default function Home() {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 0.8, duration: 0.6 }}
-          href="https://github.com/lesimoes/clip-save/releases/download/v1.0.1/clip-save.zip"
+          href={appLink}
           target="_blank"
           rel="noopener noreferrer"
           className="mt-10 px-6 py-3 border border-[#F2502F] text-[#F2502F] rounded-full text-lg hover:bg-[#F2502F] hover:text-white transition-all"
@@ -71,11 +74,11 @@ export default function Home() {
           className="w-full text-center md:w-1/2"
         >
           <h2 className="text-3xl md:text-5xl font-semibold mb-6">
-            Quick access to <Highlight>texts and shortcuts</Highlight>.
+            Quick access to <Highlight>texts and web pages</Highlight>
           </h2>
           <p className="text-lg text-gray-600">
-            Save texts and copy them to your clipboard. Save your most used apps
-            and files.
+            Save texts and copy them to your clipboard. You can save web page
+            link and open it.
           </p>
         </motion.div>
         <motion.div
@@ -87,6 +90,38 @@ export default function Home() {
         >
           <Image
             src="/screenshot.png"
+            alt="Screenshot do ClipSave"
+            width={800}
+            height={100}
+            className="rounded-xl shadow-md w-[300px]"
+          />
+        </motion.div>
+      </section>
+
+      <section className="flex flex-col  items-center justify-between px-6 md:px-12 py-24 gap-12">
+        <motion.div
+          initial={{ opacity: 0, x: -40 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.8 }}
+          className="w-full text-center md:w-1/2"
+        >
+          <h2 className="text-3xl md:text-5xl font-semibold mb-6">
+            Quick access to <Highlight>files and applications</Highlight>
+          </h2>
+          <p className="text-lg text-gray-600">
+            Create shortcuts to apps or files and open their folders directly.
+          </p>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, x: 40 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.8 }}
+          className="md:w-1/2 flex justify-center"
+        >
+          <Image
+            src="/screenshot2.png"
             alt="Screenshot do ClipSave"
             width={800}
             height={100}
